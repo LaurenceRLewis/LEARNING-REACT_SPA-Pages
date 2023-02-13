df3683b12bb64654246c98bb55b1da637c66ce18
Laurence Lewis (he/him) <laurence@a11ycats.com.au>
2023-02-13 15:19:19 +1000

    Update to include new functionality Setting focus to to a link with aria-describedby pointing to the heading

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+//Setting focus to a heading
+
+// import React, { useState } from "react";
+// import Page1 from "./components/Page1";
+// import Page2 from "./components/Page2";
+// import "./styles.css";
+
+// function App() {
+//   const [currentPage, setCurrentPage] = useState("page1");
+
+//   return (
+//     <div className="container">
+//       {currentPage === "page1" ? (
+//         <Page1 setCurrentPage={setCurrentPage} />
+//       ) : (
+//         <Page2 />
+//       )}
+//     </div>
+//   );
+// }
+
+// export default App;
+
+//Setting focus to a link
+
 import React, { useState } from "react";
 import Page1 from "./components/Page1";
 import Page2 from "./components/Page2";
 import "./styles.css";
 
-function App() {
+const App = () => {
   const [currentPage, setCurrentPage] = useState("page1");
 
   return (
@@ -15,6 +40,6 @@ function App() {
       )}
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Page2.js
+++ src/components/Page2.js
@@ -1,22 +1,52 @@
-import React, { useState, useEffect } from "react";
+//import React, { useState, useEffect } from "react";
 
-function Page2() {
-  const [status, setStatus] = useState("");
-  const statusId = "status-message";
+//Setting focus to the heading
+// function Page2() {
+//   const [status, setStatus] = useState("");
+//   const statusId = "status-message";
 
+//   useEffect(() => {
+//     setStatus("Your page is loaded");
+//     document.getElementById("page-heading").focus();
+//   }, []);
+
+//   return (
+//     <div className="page">
+//       <h1 id="page-heading" tabIndex="-1" aria-describedby={statusId}>
+//         Page 2
+//       </h1>
+//       <p id={statusId}>{status}</p>
+//     </div>
+//   );
+// }
+
+// export default Page2;
+
+//Setting focus to a link
+
+import React, { useEffect } from "react";
+
+const Page2 = ({ setCurrentPage }) => {
   useEffect(() => {
-    setStatus("Your page is loaded");
-    document.getElementById("page-heading").focus();
+    document.getElementById("back-to-page1").focus();
   }, []);
 
   return (
     <div className="page">
-      <h1 id="page-heading" tabIndex="-1" aria-describedby={statusId}>
+      <a
+        href="#page1"
+        id="back-to-page1"
+        onClick={() => setCurrentPage("page1")}
+        aria-describedby="page2-heading"
+      >
+        Previous Page
+      </a>
+      <h1 id="page2-heading" tabIndex={-1}>
         Page 2
       </h1>
-      <p id={statusId}>{status}</p>
+      <p aria-describedby="page2-heading">Your page is loaded</p>
     </div>
   );
-}
+};
 
 export default Page2;
